2ceb3749b56f20a61086b7ecbb45cececa80528c
Tona <tona.chheun@akilabs.co>
2025-08-22 12:57:32 +0700

    init

---
+++ src/main/java/com/example/spring_jpa_demo/SpringJpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaDemoApplication.class, args);
+	}
+
+}
